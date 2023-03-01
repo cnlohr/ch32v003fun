38479bc1ec80a14f7337828a62c767aaeb6d5a62
cnlohr <lohr85@gmail.com>
2023-02-28 16:51:09 -0800

    Move around minichlink stuff to make it clear.

--- minichlink/test.bat
+++ minichlink/test.bat
@@ -1,10 +1,10 @@
 
 
-tcc wch_erase.c libusb-1.0.dll
-tcc wch_reset.c libusb-1.0.dll
-tcc wch_write_simple.c libusb-1.0.dll
+rem tcc wch_erase.c libusb-1.0.dll
+rem tcc wch_reset.c libusb-1.0.dll
+rem tcc wch_write_simple.c libusb-1.0.dll
 tcc minichlink.c libusb-1.0.dll
-tcc wch_dump_flash.c libusb-1.0.dll
+rem tcc wch_dump_flash.c libusb-1.0.dll
 rem wch_erase.exe
 rem wch_write_simple.exe ..\barebones\barebones.bin
 rem wch_reset.exe
