f706b7d9d205cfacb3ec7ccde4a2f58c0da4abca
Eric Brombaugh <ebrombaugh1@cox.net>
2023-03-26 12:07:23 -0700

    Added demo for systick - not working yet.

---
+++ examples/systick_irq/systick_irq.c
@@ -0,0 +1,82 @@
+/*
+ * Example for using SysTick with IRQs
+ * 03-25-2023 E. Brombaugh
+ */
+
+// Could be defined here, or in the processor defines.
+#define SYSTEM_CORE_CLOCK 48000000
+#define APB_CLOCK SYSTEM_CORE_CLOCK
+
+#include "ch32v003fun.h"
+#include <stdio.h>
+#include "systick.h"
+
+int main()
+{
+	uint32_t count = 0;
+	
+	SystemInit48HSI();
+
+	// start serial @ default 115200bps
+	SetupUART( UART_BRR );
+	printf("\r\r\n\nsystick_irq example\n\r");
+
+	printf("SysTick_Handler = 0x%08X\n\r", SysTick_Handler);
+
+	// init systick @ 1ms rate
+	printf("initializing systick...");
+	systick_init();
+	printf("done.\n\r");
+	
+#if 1
+	// Debugging - does ISR run & inc counter?
+	// this section shows that IRQs during print will reset system, but during
+	// inf while() do not.
+	//while(1)
+	//for(int i=0;i<20;i++)
+	//{
+		//printf("i = %d ", i);
+		printf("CNT = 0x%08X ", SysTick->CNT);
+		printf("systick_cnt = 0x%08X\n\r", systick_cnt);
+	//}
+	
+	// does cnt inc?
+	//uint32_t pcnt = systick_cnt;
+	while(1)
+	{
+		//if(pcnt != systick_cnt)
+		//{
+		//	printf("systick_cnt = %d\n\r", systick_cnt);
+		//	pcnt = systick_cnt;
+		//}
+	}
+#else
+	// Testing 1ms Systick
+	// Enable GPIOs
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOC;
+
+	// GPIO D0 Push-Pull
+	GPIOD->CFGLR &= ~(0xf<<(4*0));
+	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0);
+
+	// GPIO D4 Push-Pull
+	GPIOD->CFGLR &= ~(0xf<<(4*4));
+	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4);
+
+	// GPIO C0 Push-Pull
+	GPIOC->CFGLR &= ~(0xf<<(4*0));
+	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0);
+
+	//printf("looping...\n\r");
+	while(1)
+	{
+		GPIOD->BSHR = 1 | (1<<4);	 // Turn on GPIOs
+		GPIOC->BSHR = 1;
+		systick_delay_ms( 250 );
+		GPIOD->BSHR = (1<<16) | (1<<(16+4)); // Turn off GPIODs
+		GPIOC->BSHR = (1<<16);
+		systick_delay_ms( 250 );
+		printf( "Count: %lu\n\r", count++ );
+	}
+#endif
+}
