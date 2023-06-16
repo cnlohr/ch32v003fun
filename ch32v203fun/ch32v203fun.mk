38224d271238df8bafa729c0f1336210b8a8d0e9
Alexander Mandera <alexander@mandera.eu>
2023-06-16 12:07:16 +0200

    Add linker file, Makefile and basic header

---
+++ ch32v203fun/ch32v203fun.mk
@@ -0,0 +1,54 @@
+
+PREFIX?=riscv64-unknown-elf
+
+CH32V203FUN?=../../ch32v203fun
+MINICHLINK?=../../minichlink
+
+CFLAGS+= \
+	-g -Os -flto -ffunction-sections \
+	-static-libgcc \
+	-march=rv32imafc \
+	-mabi=ilp32f \
+	-I/usr/include/newlib \
+	-I$(CH32V203FUN) \
+	-nostdlib \
+	-I. -Wall
+
+LDFLAGS+=-T $(CH32V203FUN)/ch32v203fun.ld -Wl,--gc-sections -L$(CH32V203FUN)/../misc -lgcc
+
+SYSTEM_C:=$(CH32V203FUN)/ch32v203fun.c
+
+$(TARGET).elf : $(SYSTEM_C) $(TARGET).c $(ADDITIONAL_C_FILES)
+	$(PREFIX)-gcc -o $@ $^ $(CFLAGS) $(LDFLAGS)
+
+$(TARGET).bin : $(TARGET).elf
+	$(PREFIX)-size $^
+	$(PREFIX)-objdump -S $^ > $(TARGET).lst
+	$(PREFIX)-objdump -t $^ > $(TARGET).map
+	$(PREFIX)-objcopy -O binary $< $(TARGET).bin
+	$(PREFIX)-objcopy -O ihex $< $(TARGET).hex
+
+ifeq ($(OS),Windows_NT)
+closechlink :
+	-taskkill /F /IM minichlink.exe /T
+else
+closechlink :
+	-killall minichlink
+endif
+
+terminal : monitor
+
+monitor :
+	$(MINICHLINK)/minichlink -T
+
+gdbserver : 
+	-$(MINICHLINK)/minichlink -baG
+
+cv_flash : $(TARGET).bin
+	make -C $(MINICHLINK) all
+	$(MINICHLINK)/minichlink -w $< flash -b
+
+cv_clean :
+	rm -rf $(TARGET).elf $(TARGET).bin $(TARGET).hex $(TARGET).lst $(TARGET).map $(TARGET).hex || true
+
+build : $(TARGET).bin
