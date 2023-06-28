3430c4a68f78e76bc2b974815adf820a56835e95
cnlohr <lohr85@gmail.com>
2023-06-28 05:04:45 -0400

    Oops: don't turn off printf onthe printf demo.

--- examples/debugprintfdemo/funconfig.h
+++ examples/debugprintfdemo/funconfig.h
@@ -2,9 +2,8 @@
 #define _FUNCONFIG_H
 
 // Though this should be on by default we can extra force it on.
-#define FUNCONF_USE_DEBUGPRINTF 0
+#define FUNCONF_USE_DEBUGPRINTF 1
 #define FUNCONF_DEBUGPRINTF_TIMEOUT (1<<31) // Wait for a very very long time.
-
 
 #define CH32V003           1
 
