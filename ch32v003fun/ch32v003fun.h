04a09601d0fcf35890483b118d6d84dd0700f21a
cnlohr <lohr85@gmail.com>
2024-02-15 15:06:00 -0800

    Update comment clarifying that semihosting works.

--- ch32v003fun/ch32v003fun.h
+++ ch32v003fun/ch32v003fun.h
@@ -29,8 +29,6 @@
 
 		poll_input, handle_debug_input may be used with semihsoting.
 
-		Note: Semihosting appears to currently only work on the CH32V003 (may be improved in future)
-
 	For UART printf, on:
 		CH32V003, Port D5, 115200 8n1
 		CH32V203, Port A9, 115200 8n1
