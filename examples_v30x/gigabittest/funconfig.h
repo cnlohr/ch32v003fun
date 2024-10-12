1629eac29488cea1f5c4acb02b1112d300fd611f
cnlohr <lohr85@gmail.com>
2024-10-12 13:15:30 -0700

    Add gigabit test example

---
+++ examples_v30x/gigabittest/funconfig.h
@@ -0,0 +1,10 @@
+#ifndef _FUNCONFIG_H
+#define _FUNCONFIG_H
+
+#define CH32V30x 1
+#define FUNCONF_SYSTICK_USE_HCLK 1
+#define FUNCONF_USE_DEBUGPRINTF 1
+#define FUNCONF_USE_HSE 1               // Use External Oscillator
+
+#endif
+
