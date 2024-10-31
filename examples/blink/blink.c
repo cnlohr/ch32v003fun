70b3236dfb35c05575306bc890f9e8d3ecc0f4df
dwillmore <10374140+dwillmore@users.noreply.github.com>
2024-10-31 08:16:35 -0400

    Use friendly defines for pin names

--- examples/blink/blink.c
+++ examples/blink/blink.c
@@ -1,6 +1,12 @@
 #include "ch32v003fun.h"
 #include <stdio.h>
 
+// use defines to make more meaningful names for our GPIO pins
+#define PIN_1 PD0
+#define PIN_K PD4
+#define PIN_BOB PD6
+#define PIN_KEVIN PC0
+
 int main()
 {
 	SystemInit();
@@ -8,22 +14,22 @@ int main()
 	// Enable GPIOs
 	funGpioInitAll();
 	
-	funPinMode( PD0, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
-	funPinMode( PD4, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
-	funPinMode( PD6, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
-	funPinMode( PC0, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( PIN_1,     GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( PIN_K,     GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( PIN_BOB,   GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( PIN_KEVIN, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
 
 	while(1)
 	{
-		funDigitalWrite( PD0, FUN_HIGH );
-		funDigitalWrite( PD4, FUN_HIGH );
-		funDigitalWrite( PD6, FUN_HIGH );
-		funDigitalWrite( PC0, FUN_HIGH );
+		funDigitalWrite( PIN_1,     FUN_HIGH );
+		funDigitalWrite( PIN_K,     FUN_HIGH );
+		funDigitalWrite( PIN_BOB,   FUN_HIGH );
+		funDigitalWrite( PIN_KEVIN, FUN_HIGH );
 		Delay_Ms( 250 );
-		funDigitalWrite( PD0, FUN_LOW );
-		funDigitalWrite( PD4, FUN_LOW );
-		funDigitalWrite( PD6, FUN_LOW );
-		funDigitalWrite( PC0, FUN_LOW );
+		funDigitalWrite( PIN_1,     FUN_LOW );
+		funDigitalWrite( PIN_K,     FUN_LOW );
+		funDigitalWrite( PIN_BOB,   FUN_LOW );
+	  funDigitalWrite( PIN_KEVIN, FUN_LOW );
 		Delay_Ms( 250 );
 	}
 }
